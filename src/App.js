00068bc2c6214d088f9cad0fb7b746bdaf56fb9a
RigoTheAmigo <58004513+RodGodoy@users.noreply.github.com>
2020-05-07 17:06:43 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This Is Rods Project
+          This Is Rods Project Three Edit
         </p>
         <a
           className="App-link"
