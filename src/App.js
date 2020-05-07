87600a45ff135718d34d86172a7bd2cb8eff39b9
Ubuntu <ubuntu@ip-172-31-44-25.us-east-2.compute.internal>
2020-05-07 22:31:35 +0000

    modified the app to have more words

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This Is Rods Project Three Edit
+          This Is Rods Project Three Edit For Class With More Words
         </p>
         <a
           className="App-link"
